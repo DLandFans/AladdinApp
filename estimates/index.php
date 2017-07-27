f6caafa8cadfc99a5eca03c3dd6093aa57773d60
Marc McKeown <dlandfans@gmail.com>
2017-07-26 23:05:33 -0700

    Added JQuery Handle

--- estimates/index.php
+++ estimates/index.php
@@ -10,9 +10,12 @@
         <!--[if lt IE 9]>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
         <![endif]-->
-
+        
+        <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/ar-knack-main.js"></script>
         <script type="text/javascript">app_id="5963af2e76102861995ee01e";distribution_key="dist_4";</script>
         <script type="text/javascript" src="https://loader.knack.com/5963af2e76102861995ee01e/dist_4/knack.js"></script>
+        
     </head>
     <body>
         <div id="main">
@@ -21,7 +24,5 @@
     </body>
 </html> 
  
-
-
 
 
---
+++ estimates/js/ar-knack-main.js
@@ -0,0 +1,6 @@
+function ttp_knack_page_render(event, page) {
+    //alert('listener for page: ' + page.key);
+    $('#view_31 .view-header h2').html("Super Estimating");
+}  
+
+
