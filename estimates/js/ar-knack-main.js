cbf8a1c383b799d39dba60a236ee1dea77d02d37
Marc McKeown <dlandfans@gmail.com>
2017-07-29 00:15:08 -0700

    Dom Mods Complete

--- estimates/js/ar-knack-main.js
+++ estimates/js/ar-knack-main.js
@@ -1,23 +1,78 @@
 function ttp_knack_page_render(event, page) {
-    //alert('listener for page: ' + page.key);
-    //$('#view_31 .view-header h2').html("Super Estimating");
 
-    var inner = $('.kn-crumbtrail a').toArray();
-    var estId = "";
+    //Add the estURLs to the page object
+    page.estURLs = {
+      "baseURL":"#estimates/estimate-details/",
+      "deficiencies":{
+          0:"view-general-conditions",
+          1:"view-surface-conditions",
+          2:"view-roofing-features",
+          3:"view-exterior-surfaces",
+          4:"view-interior-surfaces"
+      }
+    };
     
-    if (inner.length > 1) {
-        estId = inner[1].hash;
-        estId = estId.substr(estId.length-25,24);
-        $('.kn-crumbtrail').replaceWith('<div class="ttp-button"><a href="'  + inner[1].href + '">' + inner[1].innerHTML + '</a></div>');
-
-    } else {
-        $('.kn-crumbtrail').replaceWith("");
+    if (page.scene_id) {
+        //Change the breadcrumb
+        var breadCrumb = $('.kn-crumbtrail a').toArray();
+        $('.kn-crumbtrail').replaceWith('<div class="ttp-button"><a href="'  + breadCrumb[1].href + '">' + breadCrumb[1].innerHTML + '</a></div>');
+        
+        //Change the deficiency links
+        changeLinks(event, page);
     }
-    
-    //$('#information').replaceWith(estId);
-    
-    
-    console.log(page);
-    
+    return true;
 }  
 
+
+function changeLinks(event, page) {
+    
+    var defURL;
+    switch(page.key)
+    {
+        case "scene_23":
+            defURL = $('#kn-' + page.key + ' #view_31 div.kn-details-group:nth-child(4) a').toArray();
+            exchangeLinks(event,page,defURL, 99);
+            break;
+        case "scene_43":
+            defURL = $('#kn-' + page.key + ' #view_59 div.kn-details-group:nth-child(3) a').toArray();
+            exchangeLinks(event,page,defURL, 0);
+            break;
+        case "scene_44":
+            defURL = $('#kn-' + page.key + ' #view_60 div.kn-details-group:nth-child(3) a').toArray();
+            exchangeLinks(event,page,defURL, 1);
+            break;
+        case "scene_45":
+            defURL = $('#kn-' + page.key + ' #view_61 div.kn-details-group:nth-child(3) a').toArray();
+            exchangeLinks(event,page,defURL, 2);
+            break;
+        case "scene_46": 
+            defURL = $('#kn-' + page.key + ' #view_62 div.kn-details-group:nth-child(3) a').toArray();
+            exchangeLinks(event,page,defURL, 3);
+            break;
+        case "scene_47":
+            defURL = $('#kn-' + page.key + ' #view_63 div.kn-details-group:nth-child(3) a').toArray();
+            exchangeLinks(event,page,defURL, 4);
+            break;
+        default:
+            defURL = 'none';
+    }
+    return true;
+}
+    
+
+function exchangeLinks(event, page, defURL, whichOne) {
+    
+    var place = 0;
+    var look = 0;
+
+    while(place < defURL.length)
+    {
+        if(look !== whichOne) {
+            defURL[place].hash = page.estURLs.baseURL + page.scene_id + '/' + page.estURLs.deficiencies[look] + '/' + page.scene_id;
+            place++;
+        } 
+        look++;
+    }
+    
+    return true;
+}
